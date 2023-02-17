381f787814ebbe7560ed2588bc3ac28b80521317
MrHatcherstone <www.garry1999@gmail.com>
2023-02-18 02:28:22 +0300

    added yield() and several prints removed

--- Lightning_check_v4.0.ino
+++ Lightning_check_v4.0.ino
@@ -42,62 +42,45 @@ void staticColour(){
 }
 
 void WeelColour (){
-  Serial.println("WeelColour enter");
   if (colour <= 255) // красный макс, зелёный растёт
   { 
-    Serial.println("if 1 enter");
     int_R = 255;
     int_G = colour;
     int_B = 0;
-    Serial.println("if 1 exit");
   }
   if ((colour > 255) && (colour <= 510)) // зелёный макс, падает красный
   {
-    Serial.println("if 2 enter");
     int_R = 510 - colour;
     int_G = 255;
     int_B = 0;
-    Serial.println("if 2 exit");
   }
   if ((colour > 510) && (colour <= 765)) // зелёный макс, растёт синий
   {
-    Serial.println("if 3 enter");
     int_R = 0;
     int_G = 255;
     int_B = colour - 510;
-    Serial.println("if 3 exit");
   }
   if ((colour > 765) && (colour <= 1020)) // синий макс, падает зелёный
   {
-    Serial.println("if 4 enter");
     int_R = 0;
     int_G = 1020 - colour;
     int_B = 255;
-    Serial.println("if 4 exit");
   }
   if ((colour > 1020) && (colour <= 1275)) // синий макс, растёт красный
   {
-    Serial.println("if 5 enter");
     int_R = colour - 1020;
     int_G = 0;
     int_B = 255;
-    Serial.println("if 5 exit");
   }
   if ((colour > 1275) && (colour <= 1530)) // красный макс, падает синий
   {
-    Serial.println("if 6 enter");
     int_R = 255;
     int_G = 0;
     int_B = 1530 - colour;
-    Serial.println("if 6 exit");
   }
-  Serial.println("рассчёт яркости 1");
   int_R = int_R * ledBright / 100;
-  Serial.println("рассчёт яркости 2");
   int_G = int_G * ledBright / 100;
-  Serial.println("рассчёт яркости 3");
   int_B = int_B * ledBright / 100; 
-  Serial.println("рассчёт яркости окончен");
 
   Serial.println("setPixelColor"+String(ledPosition)+" "+String(int_R)+" "+String(int_G)+" "+String(int_B)+" "+String(colour));
   pixels.setPixelColor(ledPosition, pixels.Color(int_R, int_G, int_B));
@@ -107,27 +90,14 @@ void WeelColour (){
   Serial.println("delay"); 
   delay(ledSpeed);
 
-
-  Serial.println("if colour");
   if (colour + 3 > 1530)
-    {
       colour = 0;
-      Serial.println("if colour true");
-    }
   else
-  {
     colour += 3;
-    Serial.println("if colour false");
-  }
-  Serial.println("ledPosition");
+    
   ledPosition += 1;
-  Serial.println("if ledPosition");
   if (ledPosition >= pix_count)
-    {
-      Serial.println("if ledPosition true");
       ledPosition = 0;
-    }
-  Serial.println("WeelColour exit");
 }
 
 void WeelColourReverse (){
@@ -276,56 +246,29 @@ void setup(void){
 }
 
 void loop(void){
-  if (workIsFinished == false)
-      Serial.println("Вход Луп");
-  //yield();
-  if (workIsFinished == false)
-    Serial.println("Перед принятием клиента");
   server.handleClient();
-  if (workIsFinished == false)
-    Serial.println("После принятия клиента");
   delay(10);
-  if (workIsFinished == false)
-      Serial.println("После delay");
   if (workIsFinished == false){
-    Serial.println("внутри if");
     switch (lightningType) {
         case 0:
-        Serial.println("case 0 enter");
           workIsFinished = true;
-        Serial.println("case 0 exit");
           break;;
         case 1:
-        Serial.println("case 1 enter");
           staticColour();
-        Serial.println("case 1 exit");
           break;;
         case 2:
-        Serial.println("case 2 enter");
           WeelColour();
-        Serial.println("case 2 exit");
           break;;
         case 3:
-        Serial.println("case 3 enter");
           WeelColourReverse();
-        Serial.println("case 3 exit");
           break;;
         case 4:
-        Serial.println("case 4 enter");
           Randomizer();
-        Serial.println("case 4 exit");
           break;;
     }
-    Serial.println("после if");
     //mem info
     uint32_t free = system_get_free_heap_size();
-    if (workIsFinished == false)
-      Serial.println("первый mem");
     Serial.print("free mem after effect: ");
-    if (workIsFinished == false)
-      Serial.println("второй mem");
     Serial.println(free);
-    if (workIsFinished == false)
-      Serial.println("тертий mem");
   }
 }
